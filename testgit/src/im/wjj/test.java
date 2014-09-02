edc261c15975602cf0ac30a5578c56cc73112303
weijunjian <vsofun@gmail.com>
2014-09-02 21:52:27 +1000

    first test

---
+++ testgit/src/im/wjj/test.java
@@ -0,0 +1,24 @@
+/********************************************************************
+ * Designed and developed by Yeying (夜影设计开发)
+ * All Rights Reserved! Copyright @ 2000-2014 Yeying
+ ********************************************************************/
+package im.wjj;
+
+/**
+ * 
+ *
+ * @author Weijun Jian (jwjgz@126.com)
+ *
+ * 2 Sep 2014 9:46:22 pm
+ *
+ **/
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+          System.out.println("This is a test!");
+	}
+
+}
